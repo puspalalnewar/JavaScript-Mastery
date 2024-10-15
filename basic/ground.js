6bfee20732a3bd29136484450249197577e49307
Puspalal <puspalalnewar79@gmail.com>
2024-10-15 23:35:44 +0530

    Added basic files

---
+++ basic/ground.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+        name : "Puspalal Newar",
+        college : "APC Roy Govt. College",
+        sem : "3rd",
+        department : "Computer Science",
+    },
+    {
+        name : "Ghanashyam Newar",
+        college : "Saheed Kshudiram College",
+        sem : "3rd",
+        department : "English",
+    },
+    {
+        name : "Puspalal Newar",
+        college : "APC Roy Govt. College",
+        sem : "3rd",
+        department : "Computer Science",
+    },
+]
+
+// setTimeout(() => {
+//     console.log("Printed after 5sec");
+// }, 5000);
+// console.log("hi");
+
+setInterval(() => {
+    console.log("Hello World!!");
+}, 0);
