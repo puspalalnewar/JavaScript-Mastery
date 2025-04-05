ee4ab2cc2eabf7568482d4f6f44664cebd07a4c3
Puspalal <puspalalnewar79@gmail.com>
2025-04-05 22:00:00 +0530

    Js Output

---
+++ Js Output/script.js
@@ -0,0 +1,11 @@
+document.getElementById("demo").innerHTML = "<h2>Hello World</h2>";
+
+// Using document.write()
+// For testing purposes, it is convenient to use document.write():
+// document.write(5 + 6);
+// Using document.write() after an HTML document is loaded, will delete all existing HTML:
+// The document.write() method should only be used for testing.
+
+// window.alert(5 + 6);
+
+// window.print()
